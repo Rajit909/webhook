dfc3c5fac25d5268a1ab5092b3e6bbb33f9e7406
rajit <krajit909@gmail.com>
2025-07-02 13:21:15 +0530

    bug fix

--- src/controllers/webhook.controller.ts
+++ src/controllers/webhook.controller.ts
@@ -14,40 +14,6 @@ import axios from "axios";
  * * @returns {Promise<void>} Returns a JSON response with a success message and sets a cookie with the JWT token.
  */
 
-
-// export const handleWebhook = asyncHandler(
-//   async (req: Request, res: Response): Promise<void> => {
-//     const { url } = req.body;
-
-//     if (!url) {
-//       res.status(400).json({
-//         status: 'error',
-//         message: 'Missing "url" in request body',
-//       });
-//       return;
-//     }
-
-//     try {
-//       const response = await axios.get(url);
-
-//       res.json({
-//         status: 'success',
-//         requestedUrl: url,
-//         externalData: response.data,
-//       });
-//     } catch (error: any) {
-//       console.error('❌ Error fetching URL:', error.message);
-
-//       res.status(500).json({
-//         status: 'error',
-//         message: 'Failed to fetch data from the provided URL',
-//         error: error.message,
-//       });
-//     }
-//   }
-// );
-
-
 export const handleWebhook = asyncHandler(
   async (req: Request, res: Response): Promise<void> => {
     console.log("📩 Webhook received:", req.body);
--- src/routes/webhook/webhook.route.ts
+++ src/routes/webhook/webhook.route.ts
@@ -8,7 +8,6 @@ const router = express.Router();
  * auth routes for user authentication
  */
 router.post("/webhook", handleWebhook);
-router.get("/", handleWebhook)
 
 
 export default router;
