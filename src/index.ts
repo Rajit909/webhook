568e3d760d88d95db3da2177d8a8dc5939305592
rajit <krajit909@gmail.com>
2025-07-02 13:26:19 +0530

    bug fix

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use('/api/v1/', mainRoute);
+app.use('/api/v1', mainRoute);
 
 
 app.get('/', (_, res) => {   
--- src/routes/webhook/webhook.route.ts
+++ src/routes/webhook/webhook.route.ts
@@ -1,12 +1,8 @@
 import express from "express";
 import { handleWebhook } from "../../controllers/webhook.controller.js";
 
-
 const router = express.Router();
 
-/**
- * auth routes for user authentication
- */
 router.post("/webhook", handleWebhook);
 
 
